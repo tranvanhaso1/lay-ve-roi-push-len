14b62fa4bad9c033bcd662805b3733f8d5ace4cf
tranvanhaso1 <tranvanhaql@gmail.com>
2018-07-16 08:40:57 +0700

    upload cho header

---
+++ Test/Scripts/Script.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(window).bind('scroll', function () {
+        var navHeight = $(window).height() - 100;
+        if ($(window).scrollTop() > navHeight) {
+            $('#sticky-bar').addClass('navbar-fixed-top');
+        }
+        else {
+            $('#sticky-bar').removeClass('navbar-fixed-top');
+        }
+    });
+});
